dd6d4751a274c09c98a4cf5e889d330a997f16d4
manugeorge04 <manugeorge004@gmail.com>
2021-04-27 20:46:24 +0530

    Bug Fix

--- client/src/hoc/Header.js
+++ client/src/hoc/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Header = () => {
   return (
     <div className='header'>
-      <img className='header__image__cow' src='/images/cow.png' />
+      <img className='header__image__cow' src='images/cow.png' />
       <div className='header__container'>              
         <h1 className='header__title'>Cows and Bulls</h1>              
       </div>
-      <img className='header__image__bull' src='/images/bull.png' />
+      <img className='header__image__bull' src='images/bull.png' />
     </div>
   )
 };

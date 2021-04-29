1f4ad422527108abca106fcaf19a6b5c0a69f5b1
manugeorge04 <manugeorge04@users.noreply.github.com>
2021-04-29 17:26:12 +0000

    deploy: f847e4c5cc09feec7600c8b2d8852fa3a4545711

---
+++ dist/7.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{161:function(n,e,t){"use strict";t.r(e);var u=t(0),a=t.n(u);e.default=function(){return a.a.createElement("div",null,a.a.createElement("h1",null,"404 not found"))}}}]);
+//# sourceMappingURL=7.bundle.js.map

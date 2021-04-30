6861478166cb4217b43b2060d2fbda07278c4d4f
manugeorge04 <manugeorge004@gmail.com>
2021-04-30 14:06:16 +0530

    Added Rules for each mode

--- client/src/components/RulesCarouselCard.js
+++ client/src/components/RulesCarouselCard.js
@@ -1,30 +1,45 @@
 import React from'react';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper'
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import Slide from '@material-ui/core/Slide';
+import Typography from '@material-ui/core/Typography';
 
 import {Cowputer, Herds, Bullfight} from '../utils/rulesContent'
-
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
     display:'flex',
-    width: "50%",
+    width: "50%",    
     height:'auto',
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],    
-    top: '25%',
-    left: '25%'
+    top: '10%',
+    left: '25%',
+    [theme.breakpoints.down('sm')]: {
+      width: "90%",      
+      top: '15%',
+      left: '5%',      
+    },
   },
   content: {
     flexGrow:'2',
     padding:'2rem 0 2rem'
+  },
+  wrapIcon: {
+    verticalAlign: 'middle',
+    display: 'inline-flex'
+  },
+  Icon: {
+    width: '10%',
+    [theme.breakpoints.down('sm')]: {
+      width: "15%",            
+    },
+    height: 'auto'
   }  
 }));
 
@@ -43,11 +58,31 @@ const RulesCarouselCard = (props) => {
       >
         <ArrowBackIosIcon fontSize="large" />
       </IconButton>      
-        <div className={classes.content}>
-          {props.btnId===3 && <Cowputer />}
-          {props.btnId===2 && <Bullfight/>}
-          {props.btnId===1 && <Herds />}
-        </div>        
+
+      <div className={classes.content}>
+        {props.btnId===3 && <Cowputer />}
+        {props.btnId===2 && <Bullfight/>}
+        {props.btnId===1 && <Herds />}
+        <Typography variant="h4" align="center" paragraph={true}>
+            Enter a word and you will be told how many COWS and BULLS lie in it. Words having repeated letters will be considered invalid.
+            Eg: HOOF, EYES    
+        </Typography>        
+        <Typography variant="h4" align="center" paragraph={true} className={classes.wrapIcon} >
+          <embed  className={classes.Icon} src="images/cow.svg" />
+          COW is a correct letter
+          in the wrong position.
+          Eg: Ans- HIDE Guess- SKIN
+          I is a COW          
+        </Typography>
+        <Typography variant="h4" align="center"  className={classes.wrapIcon} >
+        <embed  className={classes.Icon} src="images/bull.svg" /> BULL is a correct letter
+          in the correct position.
+          Eg: Ans- CALF Guess- MILK
+          L is a BULL          
+        </Typography>
+
+      </div>        
+
       <IconButton 
       className={classes.button}
       onClick={props.handleOnForwardClick}
@@ -60,3 +95,7 @@ const RulesCarouselCard = (props) => {
 }
 
 export default RulesCarouselCard
+
+
+
+//className={classes.wrapIcon}
--- client/src/utils/rulesContent.js
+++ client/src/utils/rulesContent.js
@@ -3,53 +3,44 @@ import Typography from '@material-ui/core/Typography';
 
 export const Cowputer = () => {return(
   <Fragment>
-    <Typography variant="h1">
+    <Typography variant="h1" >
        Cowputer
     </Typography>
-    <Typography variant="h1">
-       Cowputer
+    <Typography variant="h3" paragraph={true}>
+      Friends being unreliabull!
     </Typography>
-    <Typography variant="h1">
-      Cowputer
-    </Typography>
-    <Typography variant="h1">
-      Cowputer
-    </Typography>
+    <Typography variant="h4" align="center" paragraph={true}>
+    The computer provides the 4-letter word that you, the player must guess. Guess it within 12 turns and you get the round!
+    </Typography>    
   </Fragment>  
 )}
 
 
 export const Bullfight = () => {return(
   <Fragment>
-    <Typography variant="h1">
-      Bullfight
+    <Typography variant="h1" >
+       Bullfight
     </Typography>
-    <Typography variant="h1">
-      Bullfight
+    <Typography variant="h3" paragraph={true}>
+       Get ready for udder chaos!
     </Typography>
-    <Typography variant="h1">
-      Bullfight
-    </Typography>
-    <Typography variant="h1">
-      Bullfight
-    </Typography>
-  </Fragment>      
+    <Typography variant="h4" align="center" paragraph={true}>
+    One player selected at random provides the 4-letter word that all other players must guess. It’s a race to the finish as all the players rush to guess the word first, but be careful, each player gets 12 guesses only!
+    </Typography>    
+  </Fragment>  
 )}
 
 
 export const Herds = () => {return(
   <Fragment>
-    <Typography variant="h1">
-      Herds
+    <Typography variant="h1" >
+       Herds
     </Typography>
-    <Typography variant="h1">
-      Herds
+    <Typography variant="h3" paragraph={true}>
+       Time for a cowoperation!
     </Typography>
-    <Typography variant="h1">
-      Herds
-    </Typography>
-    <Typography variant="h1">
-      Herds
-    </Typography>
-  </Fragment>      
+    <Typography variant="h4" align="center" paragraph={true}>
+    One random player from each team provides the 4-letter word that the other team must guess. The team that guesses the answer first wins the round. Each team gets 12 guesses in total distributed equally.
+    </Typography>    
+  </Fragment>    
 )}
